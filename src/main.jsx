5f3fff57ca11ae2cff7078cebe7b6a63ec3c0bc2
Eily <eily.coghlan@gmail.com>
2026-05-20 11:22:53 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Trends from './Trends.jsx'
+
+const path = window.location.pathname;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {path === '/trends' ? <Trends /> : <App />}
   </StrictMode>,
 )
